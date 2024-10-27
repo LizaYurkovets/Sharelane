81dc6c25c7cd2e268114dc4e40ac929f425163a5
LizaSazanovets <lizayurkovets@gmail.com>
2024-10-27 19:52:12 +0300

    init: initial commit, repo created

---
+++ src/test/java/ZipCodeTest3.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZipCodeTest3 {
+
+    @Test
+    public void test() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("123456");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+}
