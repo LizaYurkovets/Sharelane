55c0e033bf1c437e676e8884e1fa92a93cc80c42
LizaSazanovets <lizayurkovets@gmail.com>
2024-10-29 18:52:56 +0300

    change tasks

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -17,4 +17,64 @@ public class ZipCodeTest {
         Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
         browser.quit();
     }
+
+    @Test
+    public void test1() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("12345");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        boolean element = browser.findElement(By.cssSelector("")).isDisplayed();
+        Assert.assertTrue(element);
+        browser.quit();
+    }
+
+    @Test
+    public void test2() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("Test");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test3() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("123456");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test4() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test5() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("@!$@!");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
 }
